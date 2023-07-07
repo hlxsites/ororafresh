3dbeb0c0a672e5319ea951e21ccfbd17113964e7
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-07 18:58:55 +0530

    Investigating on image positioning

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -11,7 +11,6 @@ export default function decorate(block) {
     const button = document.createElement('button');
     if (!i) button.classList.add('selected');
     button.addEventListener('click', () => {
-      block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
       [...buttons.children].forEach((r) => r.classList.remove('selected'));
       [...block.children].forEach((r) => r.classList.remove('selected'));
       button.classList.add('selected');
