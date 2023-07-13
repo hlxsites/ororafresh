d10f06c2b68028584ec4b11e21ce3d5c69db7018
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-13 16:50:42 +0530

    Half way through hotspot

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
-  console.log(block);
-  [...block.children].forEach((row) => {
+  const totalrows = [...block.children];
+  console.log(totalrows);
+  [...block.children].forEach((row, r) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
       if (pic) {
@@ -12,5 +13,15 @@ export default function decorate(block) {
         }
       }
     });
+    if (r > 0) {
+      row.classList.add('hidden');
+      const columns = [...row.children];
+      const text = columns[0].innerHTML;
+      console.log(text);
+      const leftdistance = columns[1].innerHTML;
+      console.log(leftdistance);
+      const rightdistance = columns[2].innerHTML;
+      console.log(rightdistance);
+    }
   });
 }
