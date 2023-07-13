89ea69d34625dc96316705204b91f34f8c875f28
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-13 18:58:45 +0530

    Working on the hotspot

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
-  const totalrows = [...block.children];
-  console.log(totalrows);
   [...block.children].forEach((row, r) => {
     [...row.children].forEach((col) => {
       const pic = col.querySelector('picture');
@@ -13,6 +11,10 @@ export default function decorate(block) {
         }
       }
     });
+    const pic = row.children[0].querySelector('picture');
+    console.log(pic);
+    const parentdiv = pic.closest('div');
+    console.log(parentdiv);
     if (r > 0) {
       row.classList.add('hidden');
       const columns = [...row.children];
@@ -22,6 +24,10 @@ export default function decorate(block) {
       console.log(leftdistance);
       const rightdistance = columns[2].innerHTML;
       console.log(rightdistance);
+      // const nexticondiv = document.createElement('div');
+      // nexticondiv.setAttribute('class', '.icon');
+      // console.log(nexticondiv);
+      // parentdiv.appendChild(nexticondiv);
     }
   });
 }
