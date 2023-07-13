005c5be0904ba49e12e832a1f606ff05fb6d63b4
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-13 14:41:51 +0530

    Figuring 1st picture for hotspot

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -1,4 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
-  // no-op
+  console.log(block);
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          // picture is only content in column
+          picWrapper.classList.add('hotspot-img');
+        }
+      }
+    });
+  });
 }
