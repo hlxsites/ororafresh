7ef7f1b4cc4164521f7de87bc0ad54d73f013060
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-13 21:18:24 +0530

    Automating the position of Hotspots

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -13,23 +13,17 @@ export default function decorate(block) {
     });
     const pic = block.querySelector('picture');
     const parentdiv = pic.closest('div');
-    console.log(parentdiv);
     if (r > 0) {
       row.classList.add('hidden');
       const columns = [...row.children];
       const text = columns[0].innerHTML;
-      console.log(text);
       const leftdistance = columns[1].innerHTML;
-      console.log(leftdistance);
       const topdistance = columns[2].innerHTML;
-      console.log(topdistance);
       const nexticondiv = document.createElement('div');
       const finalicon = 'icon' + r;
-      console.log(finalicon);
       nexticondiv.setAttribute('class', finalicon);
       nexticondiv.style.left = leftdistance;
       nexticondiv.style.top = topdistance;
-      console.log(nexticondiv);
       parentdiv.appendChild(nexticondiv);
     }
   });
