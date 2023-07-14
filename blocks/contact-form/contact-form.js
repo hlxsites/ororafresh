c0f997eb3e45e91d594ee31bd392687bc29e6ca4
Paolo Mottadelli <paolo.moz@gmail.com>
2023-07-14 16:51:32 +0200

    using loadscript

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,10 +1,11 @@
+import { loadScript } from './lib-franklin.js';
+
 export default function decorate(block) {
-  block.innerHTML = `<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
-  <script>
+  loadScript('//js.hsforms.net/forms/embed/v2.js', () => {
     hbspt.forms.create({
       region: "na1",
       portalId: "14560092",
       formId: "af288827-9574-4a29-89eb-1b853eae1c20"
     });
-  </script>`;
+  });
 }
