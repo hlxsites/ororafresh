18c4a7433653a21f619db6eb4c78dfa46a00b362
Paolo Mottadelli <paolo.moz@gmail.com>
2023-07-14 17:16:50 +0200

    fix: linting

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,11 +1,11 @@
-import { loadScript } from '/scripts/lib-franklin.js';
+import { loadScript } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   loadScript('https://js.hsforms.net/forms/embed/v2.js', () => {
     hbspt.forms.create({
-      region: "na1",
-      portalId: "14560092",
-      formId: "af288827-9574-4a29-89eb-1b853eae1c20"
+      region: 'na1',
+      portalId: '14560092',
+      formId: 'af288827-9574-4a29-89eb-1b853eae1c20',
     });
   });
 }
