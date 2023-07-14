a2dd98ab48e8177c75e3fe8f2a486dfad78e3fc0
Paolo Mottadelli <paolo.moz@gmail.com>
2023-07-14 16:20:04 +0200

    hotspot dynamic take 2

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -1,4 +1,14 @@
-// eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
-  // no-op
+  [...block.children].forEach((row, i) => {
+    if (i > 0) {
+      const item = document.createElement('div');
+      item.classList.add('item');
+      item.style.left = [...row.children][1].textContent;
+      item.style.top = [...row.children][2].textContent;
+      row.after(item);
+      [...row.children][0].classList.add('text');
+      item.appendChild([...row.children][0]);
+      row.remove();
+    }
+  })
 }
