c1a0713b60419e2e2d7993e4899ec5a7afa48ee8
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-14 18:15:00 +0530

    working on sharpening the hotspot behaviour

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -29,7 +29,7 @@ export default function decorate(block) {
       parentdiv.appendChild(nexticondiv);
       const nextpopupdiv = document.createElement('div');
       nextpopupdiv.setAttribute('class', `popup${r}`);
-      nextpopupdiv.style.left = '-450%';
+      nextpopupdiv.style.left = '-350%';
       nextpopupdiv.style.bottom = '150%';
       nextpopupdiv.innerHTML = text;
       nexticondiv.appendChild(nextpopupdiv);
