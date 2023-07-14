0eff26aaf57fea974d6787a137529ad6c4415a85
Paolo Mottadelli <paolo.moz@gmail.com>
2023-07-14 16:35:35 +0200

    add directly to block div

--- blocks/contact-form/contact-form.js
+++ blocks/contact-form/contact-form.js
@@ -1,13 +1,7 @@
 export default function decorate(block) {
-  const target = document.createElement('div');
-  target.id = 'form-target';
-  const wrapper = document.createElement('div');
-  block.appendChild(target);
-  block.appendChild(wrapper);
-  wrapper.innerHTML = `<script charset="utf-8" type="text/javascript" src="https://js.hsforms.net/forms/embed/v2.js"></script>
+  block.innerHTML = `<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
   <script>
     hbspt.forms.create({
-      target: "#form-target",
       region: "na1",
       portalId: "14560092",
       formId: "af288827-9574-4a29-89eb-1b853eae1c20"
