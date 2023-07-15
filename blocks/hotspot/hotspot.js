cd9e4dec4a660bdb700f1b063a0a264cf39713dc
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-15 14:00:39 +0530

    enabling onclick for mobile

--- blocks/hotspot/hotspot.js
+++ blocks/hotspot/hotspot.js
@@ -3,13 +3,11 @@ export default function decorate(block) {
   [...block.children].forEach((row, r) => {
     if (r > 0) {
       const nexticondiv = document.createElement('div');
-      // nexticondiv.setAttribute('class', `icon${r}`);
       nexticondiv.style.left = [...row.children][1].textContent;
       nexticondiv.style.top = [...row.children][2].textContent;
       nexticondiv.setAttribute('data', [...row.children][0].textContent);
+      nexticondiv.addEventListener('click', () => { nexticondiv.classList.add('onclick'); });
       row.after(nexticondiv);
-      // [...row.children][0].setAttribute('class', `popup${r}`);
-      // nexticondiv.appendChild([...row.children][0]);
       row.remove();
     }
   });
