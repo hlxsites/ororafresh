69c00a647861a05d298aafa5f1f0fad690ea14dc
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-17 15:39:37 +0530

    Adding Div for Cookie Consent Manager

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -22,8 +22,14 @@ export default async function decorate(block) {
     decorateIcons(footer);
     block.append(footer);
   }
+  // Adding Div for Cookie preferences
   const cookieiconparent = document.querySelector('.footer .columns div div');
   const nextdiv = document.createElement('div');
   nextdiv.setAttribute('id', 'teconsent');
   cookieiconparent.appendChild(nextdiv);
+  // Adding Div for Cookie Consent Manager
+  const cookieconsentparent = document.querySelector('.footer');
+  const nextdiv1 = document.createElement('div');
+  nextdiv1.setAttribute('id', 'consent_blackbar');
+  cookieconsentparent.appendChild(nextdiv1);
 }
