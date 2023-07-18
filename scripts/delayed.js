9c40721619a04c9a23de67a8753a412e10b48e74
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-19 02:19:23 +0530

    Added code for Goggle Tag Manager and Google Analytics

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,25 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM, loadScript } from './lib-franklin.js';
 // eslint-disable-next-line import/no-cycle
 import { loadConsentManager } from './scripts.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 // Script for Cookie consent manager
 await loadConsentManager();
+
+// google tag manager
+const gtmId = 'GTM-WSGNHXL';
+// eslint-disable-next-line
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
+
+// google analytics
+const gaId = 'UA-106405636-1';
+loadScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, 'async', () => {
+  // eslint-disable-next-line
+  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', gaId);
+});
+
+// Adding GTM Event Listner
 
 // Adding timer for button click in the Hero Carousel
 
