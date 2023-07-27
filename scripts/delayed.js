be7502c7a4c8573f6df213c47122247112324d2e
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-27 15:08:28 +0530

    Investigating the issue

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -10,9 +10,9 @@ await loadhubspotform();
 // await loadConsentManager();
 
 // google tag manager
-// const gtmId = 'GTM-WSGNHXL';
+const gtmId = 'GTM-WSGNHXL';
 // eslint-disable-next-line
-// (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
 
 // // google analytics
 // const gaId = 'UA-106405636-1';
