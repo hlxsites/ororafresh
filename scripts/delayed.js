2698d19ea9c35d2886c29eeeb8cadd99e5633149
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-27 15:07:07 +0530

    Investigating the issue

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -7,19 +7,19 @@ sampleRUM('cwv');
 // Script for Hubspot form
 await loadhubspotform();
 // Script for Cookie consent manager
-await loadConsentManager();
+// await loadConsentManager();
 
 // google tag manager
 // const gtmId = 'GTM-WSGNHXL';
 // eslint-disable-next-line
 // (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer',gtmId);
 
-// google analytics
-const gaId = 'UA-106405636-1';
-loadScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, 'async', () => {
-  // eslint-disable-next-line
-  window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', gaId);
-});
+// // google analytics
+// const gaId = 'UA-106405636-1';
+// loadScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, 'async', () => {
+//   // eslint-disable-next-line
+//   window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', gaId);
+// });
 
 // Adding GTM Event Listner
 // eslint-disable-next-line
@@ -32,18 +32,3 @@ console.log("TrustArc Events Binding..."); var dispatched = {}; var i = self.pos
 // add more delayed functionality here
 
 // Importing script for Hubspot form implementation in contact-us page
-// const contactFormBlock = document.querySelector('.block.contact-form');
-// if (contactFormBlock) {
-//   const script = document.createElement('script');
-//   script.setAttribute('type', 'text/javascript');
-//   script.src = 'https://js.hsforms.net/forms/embed/v2.js';
-//   script.addEventListener('load', () => {
-//     // eslint-disable-next-line no-undef
-//     hbspt.forms.create({
-//       region: 'na1',
-//       portalId: '14560092',
-//       formId: 'af288827-9574-4a29-89eb-1b853eae1c20',
-//     });
-//   });
-//   contactFormBlock.append(script);
-// }
