03da118348e1ac778a155165f77a82257fc8038e
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-27 16:32:21 +0530

    investigating the issue

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -132,23 +132,4 @@ export async function loadConsentManager() {
   window.dispatchEvent(new CustomEvent('consentmanager'));
 }
 
-const contactFormBlock = document.querySelector('.block.contact-form');
-if (contactFormBlock) {
-  const script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  // script.src = 'https://js.hsforms.net/forms/embed/v2.js';
-  script.addEventListener('load', () => {
-    // eslint-disable-next-line no-undef
-    loadScript('//js.hsforms.net/forms/embed/v2.js').then(() => {
-      // eslint-disable-next-line no-undef
-      hbspt.forms.create({
-        region: 'na1',
-        portalId: '14560092',
-        formId: 'af288827-9574-4a29-89eb-1b853eae1c20',
-      });
-    });
-  });
-  contactFormBlock.append(script);
-}
-
 loadPage();
