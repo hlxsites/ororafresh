f429bcaaf15ded0a756ef341d5900ffd71bf967e
Meet Kumar Jain <meetkjain83@gmail.com>
2023-07-27 14:42:14 +0530

    Investigating the issue

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,24 +4,8 @@ import { sampleRUM, loadScript } from './lib-franklin.js';
 import { loadConsentManager } from './scripts.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-// Importing script for Hubspot form implementation
-const contactFormBlock = document.querySelector('.block.contact-form');
-if (contactFormBlock) {
-  const script = document.createElement('script');
-  script.setAttribute('type', 'text/javascript');
-  script.src = 'https://js.hsforms.net/forms/embed/v2.js';
-  script.addEventListener('load', () => {
-    // eslint-disable-next-line no-undef
-    hbspt.forms.create({
-      region: 'na1',
-      portalId: '14560092',
-      formId: 'af288827-9574-4a29-89eb-1b853eae1c20',
-    });
-  });
-  contactFormBlock.append(script);
-}
 // Script for Cookie consent manager
-await loadConsentManager();
+// await loadConsentManager();
 
 // google tag manager
 const gtmId = 'GTM-WSGNHXL';
@@ -44,3 +28,20 @@ console.log("TrustArc Events Binding..."); var dispatched = {}; var i = self.pos
 // Adding timer for button click in the Hero Carousel
 
 // add more delayed functionality here
+
+// Importing script for Hubspot form implementation in contact-us page
+const contactFormBlock = document.querySelector('.block.contact-form');
+if (contactFormBlock) {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.src = 'https://js.hsforms.net/forms/embed/v2.js';
+  script.addEventListener('load', () => {
+    // eslint-disable-next-line no-undef
+    hbspt.forms.create({
+      region: 'na1',
+      portalId: '14560092',
+      formId: 'af288827-9574-4a29-89eb-1b853eae1c20',
+    });
+  });
+  contactFormBlock.append(script);
+}
