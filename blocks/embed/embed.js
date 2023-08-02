4e21684a88a694003b39b37c0f8319d70cafa119
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-02 22:24:08 +0530

    Removing the unwanted lines

--- blocks/embed/embed.js
+++ blocks/embed/embed.js
@@ -18,12 +18,6 @@ const loadScript = (url, callback, type) => {
 
 const isDesktop = window.matchMedia('(min-width: 900px)');
 
-// const getDefaultEmbed = (url) => `<div style="left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;">
-//     <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
-//       scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
-//     </iframe>
-//   </div>`;
-
 const getDefaultEmbed = (isDesktop) ? (url) => `<div style="left: 0; width: 100%; height: 476px; position: relative; padding-bottom: 0;">
 <iframe src="${url.href}" style="border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;" allowfullscreen=""
   scrolling="no" allow="encrypted-media" title="Content from ${url.hostname}" loading="lazy">
