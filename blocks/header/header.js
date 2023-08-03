8a96377511c64cb6d82ec61dcfe672935f77048e
Meet Kumar Jain <meetkjain83@gmail.com>
2023-08-03 19:29:06 +0530

    Fixed the toggle feature of Hamburger icon via JS

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -127,13 +127,9 @@ export default async function decorate(block) {
         if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
         navSection.addEventListener('click', () => {
           if (!isDesktop.matches) {
-            console.log(navSection.getAttribute('aria-expanded'));
             const expanded = navSection.getAttribute('aria-expanded') === 'true';
-            console.log(expanded);
             toggleAllNavSections(navSections);
-            console.log(navSection.getAttribute('aria-expanded'));
             navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-            console.log(navSection.getAttribute('aria-expanded'));
             if ((navSection.querySelector('ul')) && navSection.getAttribute('aria-expanded') === 'true') document.querySelector('header').style.height = '425px';
             if ((navSection.querySelector('ul')) && navSection.getAttribute('aria-expanded') === 'false') document.querySelector('header').style.height = '321px';
           }
@@ -147,7 +143,11 @@ export default async function decorate(block) {
     hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
         <span class="nav-hamburger-icon"></span>
       </button>`;
-    hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
+    hamburger.addEventListener('click', () => {
+      toggleMenu(nav, navSections);
+      if (nav.getAttribute('aria-expanded') === 'false') document.querySelector('header').style.height = '125px';
+      if (nav.getAttribute('aria-expanded') === 'true') document.querySelector('header').style.height = '321px';
+    });
     nav.prepend(hamburger);
     nav.setAttribute('aria-expanded', 'false');
 
